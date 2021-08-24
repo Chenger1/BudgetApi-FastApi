82998458a4d9c6c1d77a8a492d091be8a9c52f16
Chenger1 <exs2199@gmail.com>
2021-08-24 14:03:47 +0300

    Add handler to get list of categories of current user

--- app/db/crud.py
+++ app/db/crud.py
@@ -19,6 +19,10 @@ async def get_user_by_username(db: Session, username: str) -> Optional[User]:
     return db.query(User).filter(User.username == username).first()
 
 
+async def get_categories_by_user_id(db: Session, user_id: int) -> list[Optional[Category]]:
+    return db.query(Category).filter(Category.user_id == user_id).all()
+
+
 async def create_instance(db: Session, data: schema.BaseModel) -> Base:
     model = models[data.__class__.__name__]
     instance = model(**data.dict())
--- app/routers/categories.py
+++ app/routers/categories.py
@@ -1,12 +1,12 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 
 from sqlalchemy.orm import Session
 
 from dependecies import get_db
 from authentication import get_current_user
 
-from db.schema import Category
-from db.crud import create_instance
+from db.schema import Category, CategoryList
+from db.crud import create_instance, get_categories_by_user_id
 
 
 router = APIRouter(
@@ -19,3 +19,10 @@ router = APIRouter(
 @router.post('/create', response_model=Category)
 async def create_category_handler(category: Category, db: Session = Depends(get_db)):
     return await create_instance(db, category)
+
+
+@router.get('/all', response_model=CategoryList)
+async def get_categories_list(request: Request, db: Session = Depends(get_db)):
+    user = request.state.user
+    categories = await get_categories_by_user_id(db, user.id)
+    return {'id': user.id, 'username': user.username, 'categories': categories}
