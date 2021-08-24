f20493f7c713628a0415d2fb2af70b6d0d01eab8
Chenger1 <exs2199@gmail.com>
2021-08-24 14:09:35 +0300

    Add handler to get category detail

--- app/routers/categories.py
+++ app/routers/categories.py
@@ -6,7 +6,7 @@ from dependecies import get_db
 from authentication import get_current_user
 
 from db.schema import Category, CategoryList
-from db.crud import create_instance, get_categories_by_user_id
+from db.crud import create_instance, get_categories_by_user_id, get_object_by_id
 
 
 router = APIRouter(
@@ -26,3 +26,8 @@ async def get_categories_list(request: Request, db: Session = Depends(get_db)):
     user = request.state.user
     categories = await get_categories_by_user_id(db, user.id)
     return {'id': user.id, 'username': user.username, 'categories': categories}
+
+
+@router.get('/detail/{category_id}', response_model=Category)
+async def get_category(category_id: int, db: Session = Depends(get_db)):
+    return await get_object_by_id(db, category_id, 'Category')
