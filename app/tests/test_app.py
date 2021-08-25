16f236e489ee06f52ef08768a4646f0208398723
Chenger1 <exs2199@gmail.com>
2021-08-25 16:34:40 +0300

    Add tests for categories

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -22,6 +22,12 @@ TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engin
 Base.metadata.create_all(bind=engine)
 
 
+user_data = {
+    'username': 'test_user',
+    'password': 'test_password'
+}
+
+
 def override_get_db():
     """ Use test db instead of real """
     db = TestingSessionLocal()
@@ -50,14 +56,79 @@ app.dependency_overrides[get_db] = override_get_db
 client = TestClient(app)
 
 
+@pytest.fixture
+def get_token():
+    response = client.post('/token/', data=user_data)
+    token = response.json().get('access_token')
+    return {'Authorization': f'Bearer {token}'}
+
+
 #  Just learn how to write async tests
 @pytest.mark.asyncio
 async def test_create_user():
-    data = {
-        'username': 'test_user',
-        'password': 'test_password'
-    }
     async with AsyncClient(app=app, base_url='http://127.0.0.1:8000') as ac:
-        response = await ac.post('/token/sign-up', json=data)
+        response = await ac.post('/token/sign-up', json=user_data)
         assert response.status_code == 200
         assert 'access_token' in response.json().keys()
+
+
+def test_authorization_error():
+    response = client.get('/users/detail/1')
+    assert response.status_code == 401
+
+
+def test_wrong_login_credentials():
+    data = {
+        'username': 'test_user',
+        'password': 'wrong_passoword'
+    }
+    response = client.post('/token/', data=data)
+    assert response.status_code == 401
+
+
+def test_success_login():
+    response = client.post('/token/', data=user_data)
+    assert response.status_code == 200
+    assert 'access_token' in response.json().keys()
+
+
+def test_get_detail(get_token):
+    response = client.get('/users/detail/1', headers=get_token)
+    assert response.status_code == 200
+    assert response.json().get('username') == 'test_user'
+
+
+def test_create_category(get_token):
+    data = {
+        'name': 'Test category',
+        'user_id': 1
+    }
+    response = client.post('/categories/create', json=data, headers=get_token)
+    assert response.status_code == 200
+    assert 'name' in response.json()
+
+
+def test_edit_category(get_token):
+    data = {'name': 'Edited test category'}
+    response = client.patch('/categories/detail/1/edit', json=data, headers=get_token)
+    assert response.status_code == 200
+    assert response.json().get('name') == 'Edited test category'
+
+
+def test_list_of_categories(get_token):
+    for index in range(5):
+        data = {
+            'name': f'Test category-{index}',
+            'user_id': 1
+        }
+        client.post('/categories/create', json=data, headers=get_token)
+
+    response = client.get('/categories/all', headers=get_token)
+    assert response.status_code == 200
+    response_data = response.json()
+    assert len(response_data.get('categories')) == 6
+
+
+def test_delete_category(get_token):
+    response = client.delete('/categories/detail/1/delete', headers=get_token)
+    assert response.status_code == 200
