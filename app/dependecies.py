0126d43b346dc710344924ea22647d9c37383bfe
Chenger1 <exs2199@gmail.com>
2021-08-25 16:06:56 +0300

    Replace getting db from middleware to dependency

--- app/dependecies.py
+++ app/dependecies.py
@@ -1,5 +1,9 @@
-from fastapi import Request
+from db.database import SessionLocal
 
 
-def get_db(request: Request):
-    return request.state.db
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI
 import uvicorn
 
-from db.database import SessionLocal, engine
+from db.database import engine
 from db.models import Base
 
 from routers import users, categories, transactions
@@ -16,17 +16,6 @@ app.include_router(users.router)
 app.include_router(auth_router)
 app.include_router(categories.router)
 app.include_router(transactions.router)
-
-
-@app.middleware("http")
-async def db_session_middleware(request: Request, call_next):
-    response = Response("Internal server error", status_code=500)
-    try:
-        request.state.db = SessionLocal()
-        response = await call_next(request)
-    finally:
-        request.state.db.close()
-    return response
 
 
 if __name__ == '__main__':
