b0edf2a4d1320bdd40151f16f879df69dd9e5d92
Chenger1 <exs2199@gmail.com>
2021-08-29 01:09:11 +0300

    Refactor main.py

--- app/main.py
+++ app/main.py
@@ -7,25 +7,19 @@ from authentication import router as auth_router
 from tortoise.contrib.fastapi import register_tortoise
 from app.database import TORTOISE_ORM
 
+app = FastAPI()
+app.include_router(users.router)
+app.include_router(auth_router)
+app.include_router(categories.router)
+app.include_router(transactions.router)
 
-def create_app() -> FastAPI:
-    app = FastAPI()
-    app.include_router(users.router)
-    app.include_router(auth_router)
-    app.include_router(categories.router)
-    app.include_router(transactions.router)
+register_tortoise(
+    app=app,
+    config=TORTOISE_ORM,
+    generate_schemas=True,
+    add_exception_handlers=True
+)
 
-    register_tortoise(
-        app=app,
-        config=TORTOISE_ORM,
-        generate_schemas=True,
-        add_exception_handlers=True
-    )
-
-    return app
-
-
-app = create_app()
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000)
